c21d634e57f6bf689db7793dd00b58c1a07ab74c
gtpepi <gtpepi@hotmail.com>
2022-06-08 16:38:21 -0300

    Update ship factory and tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,19 +1,33 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-return-assign */
 
 const Ship = (leng) => {
   // sanitizer
-  if (typeof leng !== 'number' || !Number.isInteger(leng)) return undefined;
-  const length = parseInt(leng, 10);
-  let sunk = false;
+  const rejectConditions =
+    typeof leng !== 'number' || !Number.isInteger(leng) || leng > 5 || leng < 2;
+  if (rejectConditions) return undefined;
+
   // game array
   let tiles = [...Array(leng).keys()];
-
-  console.log(tiles);
+  const length = leng;
+  const isSunk = () => {
+    let isAlive = false;
+    tiles.forEach((e) => {
+      if (e !== 'hit') isAlive = true;
+    });
+    return isAlive !== true;
+  };
+  const hit = (position) => {
+    if (tiles[position] === 'hit') return;
+    tiles.splice(position, 1, 'hit');
+  };
 
   return {
-    length
+    length,
+    isSunk,
+    tiles,
+    hit
   };
 };
 
-// Ship(22);
 export default Ship;
