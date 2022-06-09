ab7de430adf8126d6788ff86cc155f2566313d03
gtpepi <gtpepi@hotmail.com>
2022-06-08 21:06:19 -0300

    Add ship placer tool

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,8 +1,37 @@
+import Ship from './Ship';
+
 const Gameboard = () => {
   const gameArray = Array(100).fill(undefined, 0);
 
+  const placeShip = (name) => {
+    let leng;
+
+    switch (name) {
+      case 'Carrier':
+        leng = 5;
+        break;
+      case 'Battleship':
+        leng = 4;
+        break;
+      case 'Destroyer':
+        leng = 3;
+        break;
+      case 'Submarine':
+        leng = 3;
+        break;
+      default:
+        leng = 2;
+        break;
+    }
+
+    const ship = Ship(leng);
+
+    return ship;
+  };
+
   return {
-    gameArray
+    gameArray,
+    placeShip
   };
 };
 
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -7,3 +7,17 @@ test('returns something', () => {
 test('gameboard should be an array of 100', () => {
   expect(Gameboard().gameArray.length).toEqual(100);
 });
+
+const newCarrier = Gameboard().placeShip('Carrier');
+const newBattleShip = Gameboard().placeShip('Battleship');
+const newDestroyer = Gameboard().placeShip('Destroyer');
+const newSubmarine = Gameboard().placeShip('Submarine');
+const newPatrolBoat = Gameboard().placeShip('Patrol Boat');
+
+test('should create new Ships with right length', () => {
+  expect(newCarrier.length).toBe(5);
+  expect(newBattleShip.length).toBe(4);
+  expect(newDestroyer.length).toBe(3);
+  expect(newSubmarine.length).toBe(3);
+  expect(newPatrolBoat.length).toBe(2);
+});
