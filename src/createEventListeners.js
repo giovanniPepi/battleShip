9ee132686990c429ae2fbc234d16deeb2902e437
gtpepi <gtpepi@hotmail.com>
2022-06-14 08:24:28 -0300

    Add keyboard support

--- src/createEventListeners.js
+++ src/createEventListeners.js
@@ -5,6 +5,17 @@ const createEventListeners = () => {
   domQueries().playAgainButton.addEventListener('click', () => {
     window.location.reload();
   });
+
+  // focus on name window
+  const processKbdInpt = (key) => {
+    if (key === 'Enter') {
+      key.preventDefault();
+      domQueries().nameForm.submit();
+    } else domQueries().nameInpt.focus();
+  };
+  window.addEventListener('keydown', (e) => {
+    processKbdInpt(e.key);
+  });
 };
 
 export default createEventListeners;
